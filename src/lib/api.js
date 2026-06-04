ee30af8daac3dfaefdcbdac299096072ced6be29
Dabsia <dabojohnson98@gmail.com>
2026-06-04 02:07:38 +0100

    Made changes to the payment structure of the frontend

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,2 +1,4 @@
-export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+export const API_BASE_URL =
+  import.meta.env.VITE_API_BASE_URL || "https://bakeease-backend.onrender.com";
+
 export const API_URL = `${API_BASE_URL}/api/v1`;
